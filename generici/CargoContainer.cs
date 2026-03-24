5ea3369d4695e08927a76cac3eeab6378c4a590f
dzredkonazar <nazar.redko.25@kse.org.ua>
2026-03-24 20:28:41 +0200

    II milestone |CargoContainer|

---
+++ generici/CargoContainer.cs
@@ -0,0 +1,24 @@
+﻿namespace generici;
+
+public class CargoContainer<T> where T : DeliveryItem
+
+{
+    private List<T> _items = new List<T>();
+
+    public void AddItem(T item)
+    {
+        _items.Add(item);
+        Console.WriteLine($"Added: {item.TrackingNumber}");
+    }
+    
+    public double GetTotalCost()
+    {
+        double total_sum = 0;
+        foreach (var  item in _items)
+        {
+            total_sum += item.CalculateCost();
+        }
+
+        return total_sum;
+    }
+}
