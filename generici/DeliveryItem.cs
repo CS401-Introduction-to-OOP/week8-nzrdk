5a7e1caa7c7d4955e625272c1d2ce55741a15a2d
dzredkonazar <nazar.redko.25@kse.org.ua>
2026-03-24 19:52:37 +0200

    I milestone

---
+++ generici/DeliveryItem.cs
@@ -0,0 +1,22 @@
+﻿namespace generici;
+
+public abstract class DeliveryItem
+{
+    public string TrackingNumber { get; set; }
+    public double Weight { get; set; }
+
+    public DeliveryItem(string trackingNumber, double weight)
+    {
+        TrackingNumber = trackingNumber;
+        Weight = weight;
+    }
+
+    public abstract double CalculateCost();
+
+    public virtual void PrintInfo()
+    {
+        string info = $"Tracking number is{TrackingNumber} \n Weight is {Weight}";
+        Console.WriteLine(info);
+    }
+
+}
---
+++ generici/Letter.cs
@@ -0,0 +1,17 @@
+﻿namespace generici;
+
+public class Letter : DeliveryItem
+{
+    public string Design { get; set; }
+
+    public Letter(string trackingNumber, double weight) : base(trackingNumber, weight)
+    
+    {
+        
+    }
+
+    public override double CalculateCost()
+    {
+        return 15 + (Weight) * 10;
+    }
+}
